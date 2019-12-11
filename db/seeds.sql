a2052cc507979414ac4c0a7d61f54970907f9a3a
Adam Baker <albcodingbootcamp@gmail.com>
2019-12-11 15:20:39 -0500

    finished db files and connection. working on orm and controller

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Southwest burger');
